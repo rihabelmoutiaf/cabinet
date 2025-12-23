350e9de086b151ac656ff9693618b437092ac6fa
ilham <ilhamoufdjikh@gmail.com>
2025-12-23 13:24:32 +0100

    Update CertificatRepo.java

--- src/main/java/ma/project/dentalTech/repository/modules/dossierMedical/api/CertificatRepo.java
+++ src/main/java/ma/project/dentalTech/repository/modules/dossierMedical/api/CertificatRepo.java
@@ -1,4 +1,16 @@
 package ma.project.dentalTech.repository.modules.dossierMedical.api;
 
-public class CertificatRepo {
+import ma.project.dentalTech.entities.dossierMedical.Certificat;
+import ma.project.dentalTech.repository.common.CrudRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface CertificatRepo extends CrudRepository<Certificat, Long> {
+
+    List<Certificat> findByPatientId(Long patientId);
+
+    List<Certificat> findByConsultationId(Long consultationId);
+
+    List<Certificat> findByDateDebutBetween(LocalDate debut, LocalDate fin);
 }
