6578a68c5ffc33af5173c5b9c41ed8ceb1088b1c
ilham <ilhamoufdjikh@gmail.com>
2025-12-23 13:27:53 +0100

    Update MedicamentRepo.java

--- src/main/java/ma/project/dentalTech/repository/modules/dossierMedical/api/MedicamentRepo.java
+++ src/main/java/ma/project/dentalTech/repository/modules/dossierMedical/api/MedicamentRepo.java
@@ -1,4 +1,15 @@
 package ma.project.dentalTech.repository.modules.dossierMedical.api;
 
-public class MedicamentRepo {
+import ma.project.dentalTech.entities.dossierMedical.Medicament;
+import ma.project.dentalTech.repository.common.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MedicamentRepo extends CrudRepository<Medicament, Long> {
+
+    Optional<Medicament> findByNom(String nom);
+
+    List<Medicament> searchByNom(String keyword);
 }
+
