12702849f43d308f22c66f3cee9231261b6c6f9f
ilham <ilhamoufdjikh@gmail.com>
2025-12-23 13:26:24 +0100

    Update DossierMedicalRepo.java

--- src/main/java/ma/project/dentalTech/repository/modules/dossierMedical/api/DossierMedicalRepo.java
+++ src/main/java/ma/project/dentalTech/repository/modules/dossierMedical/api/DossierMedicalRepo.java
@@ -1,4 +1,16 @@
 package ma.project.dentalTech.repository.modules.dossierMedical.api;
 
-public class DossierMedicalRepo {
+import ma.project.dentalTech.entities.dossierMedical.DossierMedical;
+import ma.project.dentalTech.repository.common.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DossierMedicalRepo extends CrudRepository<DossierMedical, Long> {
+
+    Optional<DossierMedical> findByPatientId(Long patientId);
+
+    boolean existsByPatientId(Long patientId);
+
+    List<DossierMedical> findByMedecinId(Long medecinId);
 }
