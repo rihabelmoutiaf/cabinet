58a1f5fd97ca5e685d79b4e5bef18c39016f091c
ilham <ilhamoufdjikh@gmail.com>
2025-12-23 13:23:52 +0100

    dossierMedical repos

--- src/main/java/ma/project/dentalTech/repository/modules/dossierMedical/api/ActeRepository.java
+++ src/main/java/ma/project/dentalTech/repository/modules/dossierMedical/api/ActeRepository.java
@@ -1,4 +1,16 @@
 package ma.project.dentalTech.repository.modules.dossierMedical.api;
 
-public class ActeRepository {
+import ma.project.dentalTech.entities.dossierMedical.Acte;
+import ma.project.dentalTech.repository.common.CrudRepository;
+
+import java.util.List;
+
+public interface ActeRepository extends CrudRepository<Acte, Long> {
+
+    List<Acte> findByConsultationId(Long consultationId);
+
+    List<Acte> findByTypeActe(String typeActe);
+
+    double sumTarifByConsultation(Long consultationId);
 }
+
