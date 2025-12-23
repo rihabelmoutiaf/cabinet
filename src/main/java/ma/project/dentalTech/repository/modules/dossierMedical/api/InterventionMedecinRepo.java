331509f15c0f49dc7e86782f0b4839eb5de167eb
ilham <ilhamoufdjikh@gmail.com>
2025-12-23 13:27:11 +0100

    Update InterventionMedecinRepo.java

--- src/main/java/ma/project/dentalTech/repository/modules/dossierMedical/api/InterventionMedecinRepo.java
+++ src/main/java/ma/project/dentalTech/repository/modules/dossierMedical/api/InterventionMedecinRepo.java
@@ -1,4 +1,14 @@
 package ma.project.dentalTech.repository.modules.dossierMedical.api;
 
-public class InterventionMedecinRepo {
+import ma.project.dentalTech.entities.dossierMedical.InterventionMedecin;
+import ma.project.dentalTech.entities.enums.RoleType;
+import ma.project.dentalTech.repository.common.CrudRepository;
+
+import java.util.List;
+
+public interface InterventionMedecinRepo extends CrudRepository<InterventionMedecin, Long> {
+
+    List<InterventionMedecin> findByConsultationId(Long consultationId);
+
+    List<InterventionMedecin> findByRole(RoleType role);
 }
