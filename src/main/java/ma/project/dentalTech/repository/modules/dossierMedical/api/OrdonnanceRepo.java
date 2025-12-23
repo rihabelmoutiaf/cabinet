ae39c4e4fd2721e768fa79949e69dbbaf0e16eb4
ilham <ilhamoufdjikh@gmail.com>
2025-12-23 13:28:40 +0100

    Update OrdonnanceRepo.java

--- src/main/java/ma/project/dentalTech/repository/modules/dossierMedical/api/OrdonnanceRepo.java
+++ src/main/java/ma/project/dentalTech/repository/modules/dossierMedical/api/OrdonnanceRepo.java
@@ -1,4 +1,14 @@
 package ma.project.dentalTech.repository.modules.dossierMedical.api;
 
-public class OrdonnanceRepo {
+import ma.project.dentalTech.entities.dossierMedical.Ordonnance;
+import ma.project.dentalTech.repository.common.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OrdonnanceRepo extends CrudRepository<Ordonnance, Long> {
+
+    List<Ordonnance> findByConsultationId(Long consultationId);
+
+    Optional<Ordonnance> findByConsultation(Long consultationId);
 }
